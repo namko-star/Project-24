516e3caf86420dc5269894b8823190e36a386506
namko-star <70060024+namko-star@users.noreply.github.com>
2020-09-19 12:51:18 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,27 @@
+class Paper {
+    constructor (x, y, radius) {
+        var paper_options = {
+            isStatic: false, 
+            restitution: 0.3,
+            friction: 0.5,
+            density: 1.2
+        }
+
+        this.x = x;
+        this.y = y;
+        this.r = radius;
+        this.body = Bodies.circle(this.x, this.y, this.r/2, paper_options);
+        World.add(world, this.body);
+    }
+
+    display() {
+       var paperPosition = this.body.position;
+       push();
+       translate(paperPosition.x, paperPosition.y);
+       rectMode(CENTER);
+       strokeWeight(3);
+       fill(255, 0, 255);
+       ellipse (0, 0, this.r, this.r);
+       pop();
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,47 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var world;
+var dustbinObject, paperObject, ground;
+
+function setup() {
+	createCanvas(1600, 700);
+	
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	paperObject = new Paper(200, 450, 40);
+	World.add(world, paperObject);
+	dustbinObject = new Dustbin(1200,635);
+	World.add(world, dustbinObject);
+	ground = new Ground(width/2, 650, width, 10, {isStatic: true});
+	World.add(world, ground);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  paperObject.display();
+  ground.display();
+  dustbinObject.display();
+
+  drawSprites();
+  keyPressed();
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		Matter.Body.applyForce(paperObject.body, paperObject.body.position, {x:85, y:-85});
+	}
+}
+
+
